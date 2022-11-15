55438463566e44bbf5e70dcd73f8e7d6e9a56d79
BerrinAKAN <61009480+BerrinAKAN@users.noreply.github.com>
2022-11-15 09:48:13 +0300

    Update UserRepository.java

--- backendproject/backendproject/src/main/java/com/digitopia/backendproject/repository/UserRepository.java
+++ backendproject/backendproject/src/main/java/com/digitopia/backendproject/repository/UserRepository.java
@@ -6,5 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
+  @Query("select u from users u where u.email like concat('%',:query, '%') ")
+    List<User> searchEmail(String query);
+
+    @Query("select u from users u where u.normalized_name like concat('%',:query, '%') ")
+    List<User> searchNormalizedName(String query);
 
 }
